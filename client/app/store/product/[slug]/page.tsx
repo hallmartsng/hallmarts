be47a2f372625d37aa966416697dbd39d8b25ce4
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2026-04-23 00:34:29 +0100

    fix(params): Page prop params typing

--- client/app/store/product/[slug]/page.tsx
+++ client/app/store/product/[slug]/page.tsx
@@ -1,14 +1,11 @@
 import StoreProductDetails from "@/components/store/StoreProductDetailsPage/StoreProductDetails";
-import React from "react";
 
-interface PageProps {
-  params: {
-    slug: string;
-  };
-}
-export default function ProductPage({ params }: PageProps) {
-  const { slug } = params;
-  const productId = slug.split("-").pop(); // get last part
+export default async function ProductPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const productId = params.slug.split("-").pop();
 
   return (
     <section className="flex items-center flex-col w-full gap-6 sm:pt-0 pt-10 ms:px-0 px-4">
